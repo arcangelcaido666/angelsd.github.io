40193e2454f4543b7b482c9c5fde8026eb403e9a
angelsdecon <166234941+angelsdecon@users.noreply.github.com>
2026-02-12 12:06:22 +0000

    Deploying to gh-pages from @ angelsdecon/angelsdecon.github.io@c7c4f0c8511fe526ec0a7b8437fea25c34eb974b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,9 +37,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/repositories/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+        },{id: "nav-curriculum-vitae",
+          title: "curriculum vitae",
+          description: "what i&#39;ve been doing so far! i&#39;d love to do so many more things!",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
