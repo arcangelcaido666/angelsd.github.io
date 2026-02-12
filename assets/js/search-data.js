8ca49c63df090e65dad634359f9626b1c03ea991
angelsdecon <166234941+angelsdecon@users.noreply.github.com>
2026-02-12 16:28:06 +0000

    Deploying to gh-pages from @ angelsdecon/angelsdecon.github.io@014e96ed69819fcaea88b7496a9eb5fee5541db0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-a-post-with-plotly-js",
+        },{id: "post-título-de-tu-artículo-tal-cual-sale-en-substack",
+        
+          title: 'Título de tu artículo tal cual sale en Substack <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        
+        description: "Un pequeño resumen o la primera frase de tu artículo...",
+        section: "Posts",
+        handler: () => {
+          
+            window.open("https://litoangel.substack.com/p/el-link-de-tu-post-concreto", "_blank");
+          
+        },
+      },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
