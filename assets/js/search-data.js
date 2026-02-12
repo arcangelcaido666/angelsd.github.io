879e4def27b838d5e8c473e9a708fa82197e69bd
angelsdecon <166234941+angelsdecon@users.noreply.github.com>
2026-02-12 11:17:20 +0000

    Deploying to gh-pages from @ angelsdecon/angelsdecon.github.io@7d34c22f8a0fcc58d239bbc308d91fb17dc54ee6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,15 +440,15 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "news-welcome",
+          section: "News",},{id: "news-hola-ana",
+          title: 'Hola Ana!!',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "news-welcome",
           title: 'Welcome!',
           description: "",
           section: "News",},{id: "projects-project-1",
