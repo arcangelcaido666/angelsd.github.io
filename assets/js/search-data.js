def6d6b34397cc659624f338f829b84557a11d86
angelsdecon <166234941+angelsdecon@users.noreply.github.com>
2026-02-12 15:19:39 +0000

    Deploying to gh-pages from @ angelsdecon/angelsdecon.github.io@0f0eabb4e2daa91400c2be77dc22aa542f5759e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,13 +37,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "a growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-repositories",
           title: "repositories",
           description: "edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
